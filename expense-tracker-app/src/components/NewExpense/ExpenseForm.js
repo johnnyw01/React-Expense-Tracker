3f6dea09801752fcef6d5e757d6b57b93a64b101
Johnny Wells <johnnyw01github@gmail.com>
2023-06-09 11:47:05 -0500

    Updated the ExpenseForm.js file to use useState in order to handle the individual states and state changes of the form (title, amount, and date). Implemented logic to capture the values being inputed into the form from the user, prevent the form from performing its default behavior when the Add Expense button is clicked, and implemented two-way binding that both sets and updates the forms data based upon state, state change, and user inputs, as well as empties the form after the Add Expense button is clicked.

--- expense-tracker-app/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker-app/src/components/NewExpense/ExpenseForm.js
@@ -1,29 +1,84 @@
 import React from "react";
 import "./ExpenseForm.css";
+import {useState} from "react";
 
 const ExpenseForm = () => {
+   const [enteredTitle, setEnteredTitle] = useState('');
+   const [enteredAmount, setEnteredAmount] = useState('');
+   const [enteredDate, setEnteredDate] = useState('');
+
+    //Alternative Single-State Syntax instead of the multi-state syntax above
+   // const [userInput, setUserInput] = useState({
+   //      enteredTitle: '',
+   //      enteredAmount: '',
+   //      enteredDate: ''
+   //  });
+    const titleChangeHandler = (event) => {
+        console.log(event.target.value);
+        setEnteredTitle(event.target.value);
+
+        //Alternative single-state syntax that uses the most recent previous state and only modifies a specific KVP within the state object
+        // setUserInput((prevState)=>{
+        //     return {...prevState, enteredTitle: event.target.value};
+        // });
+    };
+    const amountChangeHandler = (event) => {
+        console.log(event.target.value);
+        setEnteredAmount(event.target.value);
+
+        //Alternative single-state syntax that uses the most recent previous state and only modifies a specific KVP within the state object
+        // setUserInput((prevState)=>{
+        //     return {...prevState, enteredAmount: event.target.value};
+        // });
+    };
+    const dateChangeHandler = (event) => {
+        console.log(event.target.value);
+        setEnteredDate(event.target.value);
+
+        //Alternative single-state syntax that uses the most recent previous state and only modifies a specific KVP within the state object
+        // setUserInput((prevState)=>{
+        //     return {...prevState, enteredDate: event.target.value};
+        // });
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        console.log(expenseData);
+
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+
+    };
+
     return (
-        <form action="">
+        <form onSubmit={submitHandler} action="">
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label htmlFor="">Title</label>
-                    <input type="text"/>
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label htmlFor="">Amount</label>
-                    <input type="number" min="0.01" step="0.01"/>
+                    <input type="number" min="0.01" step="0.01" value={enteredAmount}  onChange={amountChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label htmlFor="">Date</label>
-                    <input type="date" min="2020-01-01" max="2029-12-31"/>
+                    <input type="date" min="2020-01-01" max="2029-12-31" value={enteredDate} onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__actions">
-                <button type="submit">Add Expense</button>
+                <button type="submit" >Add Expense</button>
             </div>
         </form>
     );
