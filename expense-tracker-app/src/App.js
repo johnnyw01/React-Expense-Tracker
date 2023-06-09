03a07741a5e2346dcf08751d0b03983af3b736f0
Johnny Wells <johnnyw01github@gmail.com>
2023-06-09 10:42:41 -0500

    Updated the ExpenseItem.js file to use useState and dynamically update the title of the expense item when the change title button is clicked. In addition, created the NewExpense and ExpenseForm files. The ExpenseForm.js is a form that allow a user to add a new expense item to the tracker. The NewExpense.js file is simply a funnel that feeds the form logic and future user inputs and state changes to the App.js file in order to keep the JSX code in the NewExpense.js file lean.

--- expense-tracker-app/src/App.js
+++ expense-tracker-app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
             amount: 94.12,
             date: new Date(2020, 7, 14),
         },
-        { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+        {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
         {
             id: 'e3',
             title: 'Car Insurance',
@@ -26,11 +27,12 @@ function App() {
         },
     ];
 
-  return (
-    <div>
-      <Expenses items={expenses} />
-    </div>
-  );
+    return (
+        <div>
+            <NewExpense/>
+            <Expenses items={expenses}/>
+        </div>
+    );
 }
 
 export default App;
--- expense-tracker-app/src/components/Expenses/ExpenseItem.js
+++ expense-tracker-app/src/components/Expenses/ExpenseItem.js
@@ -2,17 +2,28 @@ import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import './ExpenseItem.css';
 import Card from "../UI/Card";
+import {useState} from "react";
 
 const ExpenseItem = (props) => {
 
-  return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-    </Card>
-  );
+    const [title, setTitle] = useState(props.title);
+    console.log("ExpenseItem was executed by React")
+
+    const clickHandler = () => {
+        setTitle("Updated");
+        console.log(title)
+        return title
+    }
+
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={props.date}/>
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+            <button onClick={clickHandler}>Change title</button>
+        </Card>
+    );
 }
 export default ExpenseItem
