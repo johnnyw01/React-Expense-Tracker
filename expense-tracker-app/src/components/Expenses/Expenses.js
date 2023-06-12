1be2ad7936c732d1d5db27a5af7190f8a81adf97
Johnny Wells <johnnyw01github@gmail.com>
2023-06-12 10:15:34 -0500

    Created a new ExpensesList component and moved all of the previous built conditionally rendered logic into the new ExpensesList component and refactored it so that it returns an unordered list if there are expense items for the selected filtered year and an H2 element if there are no expense items for that selected year.

--- expense-tracker-app/src/components/Expenses/Expenses.js
+++ expense-tracker-app/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, {useState} from "react";
 import './Expenses.css';
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
 
@@ -17,24 +17,12 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    let expenseContent = <p>No expenses found...</p>;
-
-    if(filteredExpenses.length > 0){
-        expenseContent = filteredExpenses.map((expense) => (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}/>
-        ));
-
-    }
 
     return (
         <div>
             <Card className="expenses">
                 <ExpenseFilter startYear={filteredYear} onChangeFilterYear={filterChangeHandler}/>
-                {expenseContent}
+                <ExpensesList items={filteredExpenses}/>
             </Card>
         </div>
     );
