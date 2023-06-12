6f7df1607c44924087e77f02425023baf57e98b0
Johnny Wells <johnnyw01github@gmail.com>
2023-06-12 09:10:42 -0500

    Refactored the hard coded ExpenseItem data in Expenses.js file into a dynamically rendered array that uses the map method to loop each expense item and render it on the page.

--- expense-tracker-app/src/components/Expenses/Expenses.js
+++ expense-tracker-app/src/components/Expenses/Expenses.js
@@ -6,7 +6,7 @@ import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
 
-    const [filteredYear, setFilteredYear]= useState('2020');
+    const [filteredYear, setFilteredYear] = useState('2020');
     const filterChangeHandler = (selectedYear) => {
         // console.log('Expenses.js')
         // console.log(selectedYear);
@@ -17,26 +17,12 @@ const Expenses = (props) => {
         <div>
             <Card className="expenses">
                 <ExpenseFilter startYear={filteredYear} onChangeFilterYear={filterChangeHandler}/>
-                <ExpenseItem
-                    title={props.items[0].title}
-                    amount={props.items[0].amount}
-                    date={props.items[0].date}
-                />
-                <ExpenseItem
-                    title={props.items[1].title}
-                    amount={props.items[1].amount}
-                    date={props.items[1].date}
-                />
-                <ExpenseItem
-                    title={props.items[2].title}
-                    amount={props.items[2].amount}
-                    date={props.items[2].date}
-                />
-                <ExpenseItem
-                    title={props.items[3].title}
-                    amount={props.items[3].amount}
-                    date={props.items[3].date}
-                />
+                {props.items.map((expense) => (
+                    <ExpenseItem
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}/>
+                ))}
             </Card>
         </div>
     );
