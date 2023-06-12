5552dfc22f8ce3937c769ced2ad819f18df57b59
Johnny Wells <johnnyw01github@gmail.com>
2023-06-12 09:39:31 -0500

    Updated Expenses.js file to handel filtering the expense items based upon the year the user selects for.

--- expense-tracker-app/src/components/Expenses/Expenses.js
+++ expense-tracker-app/src/components/Expenses/Expenses.js
@@ -13,11 +13,15 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
     return (
         <div>
             <Card className="expenses">
                 <ExpenseFilter startYear={filteredYear} onChangeFilterYear={filterChangeHandler}/>
-                {props.items.map((expense) => (
+                {filteredExpenses.map((expense) => (
                     <ExpenseItem
                         key={expense.id}
                         title={expense.title}
